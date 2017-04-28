c468852c2fbdf8ee689e65560e343205e7d22f72
yaodingw <yaodingw@gmail.com>
2017-04-28 15:15:23 +0800

    创建sql文件夹，创建插入语句的py

--- DSregx/insertCollegeCutOff.py
+++ DSregx/insertCollegeCutOff.py
@@ -1,2 +1,19 @@
+import os
 #college_cutoff
 #get all file collegecutoff
+def findCollegeCutOff(path):
+	for x in os.listdir(path):
+		print(os.path.splitext(x)[0])
+
+		
+def getDataSource(sDataPath):
+	with open(sDataPath,"r",encoding='utf-8') as f:
+		return f.read()
+
+
+def main():
+	path="/home/wangyd/yaodingwork/biyesheji/dataSource/datatext/allCollege/allCollegeSL"
+	findCollegeCutOff(path)
+
+if __name__ == '__main__':
+	main()
